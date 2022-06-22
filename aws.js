9cc662fc3ad1f659005ce3a1727455145a029fa0
chad350 <yyc2999@gmail.com>
2022-06-22 18:10:46 +0900

    필요없는 코드 제거

--- aws.js
+++ aws.js
@@ -1,14 +1,9 @@
 import aws from 'aws-sdk';
-// import {Credentials} from 'aws-sdk';
 import env from "dotenv";
 
 env.config();
 
 const {AWS_ACCESS_KEY, AWS_SECRET_KEY} = process.env;
-
-// const access = new Credentials({
-//
-// });
 
 export const s3 = new aws.S3({
     accessKeyId: AWS_ACCESS_KEY,
@@ -16,20 +11,3 @@ export const s3 = new aws.S3({
     region: "ap-northeast-2",
     signatureVersion:'v4',
 })
-
-export function getSignedUrl({key}) {
-    return new Promise((resolve, reject)=>{
-        s3.createPresignedPost({
-            Bucket: "chad-image-upload",
-            Fields: {key},
-            Expires: 10 * 60 * 60,
-            Conditions: [
-                ["content-length-range", 0, 50 * 1024 * 1024],
-                ["starts-with", "$Content-Type", "image/"]
-            ]
-        }, (err, data) => {
-            if(err) reject(err);
-            resolve(data);
-        })
-    })
-}
--- routes/postRouter.js
+++ routes/postRouter.js
@@ -1,11 +1,5 @@
 import upload from "../middleware/imageUpload.js";
 import {Router} from 'express';
-import fs from "fs";
-import {promisify} from "util";
-import mongoose from "mongoose";
-import {s3} from '../aws.js';
-import {v4 as uuid} from "uuid";
-import mime from "mime-types";
 import Post from "../model/post.js";
 const router = Router();
 
@@ -27,8 +21,8 @@ router.post('/accommodations', upload.array("images", 5), async (req, res) => {
             people : parseInt(people),
             room : parseInt(room),
 
-            wifi : wifi === "true" ? true : false,
-            parking : parking === "true" ? true : false,
+            wifi : wifi === "true",
+            parking : parking === "true",
 
             images : req.files.map((file) => {
                 return file.location;
@@ -56,16 +50,6 @@ router.get('/accommodations', async (req, res) => {
     }
 })
 
-router.get('/accommodations', async (req, res) => {
-    try {
-        const posts = await Post.find();
-        res.json(posts);
-    } catch (e) {
-        console.log(e);
-        res.status(400).json({message: e.message});
-    }
-})
-
 router.get('/accommodations/:category', async (req, res) => {
     try {
         const {category} = params;
@@ -75,48 +59,6 @@ router.get('/accommodations/:category', async (req, res) => {
         if (!posts) throw new Error("카테고리가 잘못 됐습니다.")
 
         res.json(posts);
-
-    } catch (e) {
-        console.log(e);
-        res.status(400).json({message: e.message});
-    }
-})
-
-router.get("/me", async (req, res) => {
-    try {
-        const {lastid} = req.query;
-        if (lastid && !mongoose.isValidObjectId(lastid))
-            throw new Error("invalid lastid")
-
-        const {user, headers} = req;
-        if (!user) throw new Error("권한이 없습니다.");
-
-        const images = await Image
-            .find(lastid ?
-                {"user._id": user.id, _id: {$lt: lastid}} :
-                {"user._id": user.id})
-            .sort({_id: -1})
-            .limit(8);
-        res.json(images)
-    } catch (e) {
-        console.log(e);
-        res.status(400).json({message: e.message});
-    }
-})
-
-router.get('/:imageId', async (req, res) => {
-    try {
-        const {user, params} = req;
-        const {imageId} = params;
-
-        if (!mongoose.isValidObjectId(imageId)) throw new Error("올바르지 않은 이미지 아이디입니다.")
-
-        const image = await Image.findOne({_id: imageId})
-
-        if (!image) throw new Error("요청하신 이미지가 없습니다.")
-        if (!image.public && (!user || user.id !== image.user.id)) throw new Error("권한이 없습니다.");
-
-        res.json(image);
 
     } catch (e) {
         console.log(e);
